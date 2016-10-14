7e36eceb1b0c7f72f49678d2d195c0707358c2c6
Hexad GmbH <kanistha.a@gmail.com>
2016-10-14 13:39:24 +0200

    Initial Commit

---
+++ src/main/java/com/myapp/config/Application.java
@@ -0,0 +1,23 @@
+package com.myapp.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.myapp")
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+
+
+}
